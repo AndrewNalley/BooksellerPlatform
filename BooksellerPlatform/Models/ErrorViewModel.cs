b40d016a7a7b281f07125afa04043e2a667ac8a1
Andrew Nalley <awnalley@gmail.com>
2024-01-25 19:34:50 -0600

    first commit

---
+++ BooksellerPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BooksellerPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
