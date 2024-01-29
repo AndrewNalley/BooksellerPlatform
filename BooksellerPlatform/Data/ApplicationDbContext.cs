5996c6e328cb09183401861362a38daff848dc6b
Andrew Nalley <awnalley@gmail.com>
2024-01-29 16:53:21 -0600

    connect db to azure data studio using docker container. add basic styling template. update gitignore to remove connection string

---
+++ BooksellerPlatform/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using BooksellerPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksellerPlatform.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet <Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Mystery", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Romance", DisplayOrder = 3 }
+            );
+        }
+    }
+}
