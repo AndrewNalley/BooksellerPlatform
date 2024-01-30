59a56978fa2fc73a27ee72fc45d4f3a1dd9733e8
Andrew Nalley <awnalley@gmail.com>
2024-01-30 17:42:20 -0600

    add backend validation for categories

--- BooksellerPlatform/Controllers/CategoryController.cs
+++ BooksellerPlatform/Controllers/CategoryController.cs
@@ -16,5 +16,25 @@ namespace BooksellerPlatform.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            // if (obj.Name == obj.DisplayOrder.ToString())
+            // {
+            //     ModelState.AddModelError("name", "Display Order cannot match the name.");
+            // }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
